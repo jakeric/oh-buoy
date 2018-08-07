ec930ce833b3a10e8b40399aa0c21b87476d8aa0
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-07 17:20:24 +0100

    added seed for 20 buoys with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating 20 buoys"
+
+20.times do
+  buoy = Buoy.new(
+     title: Faker::FunnyName.name,
+     age: rand(1..67),
+     description: Faker::Demographic.race,
+     category: ["Food", "Animal", "Unicorn", "Cartoon", "Classic", "Weird"].sample,
+     capacity: rand(1..10),
+     price_per_day: rand(1..99),
+     street: Faker::Address.street_name,
+     number: rand(1..56),
+     city: Faker::Address.city,
+     country: Faker::Address.country,
+     postal_code: Faker::Address.postcode,
+    )
+  buoy.user = User.last
+  buoy.save!
+end
+
+puts "created 20 buoys - yeah buddy!"
