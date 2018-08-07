02e665fc1f2c9f43fecd11fd34437255035ff33c
Scarlett Wolter <scarlett.wolter@web.de>
2018-08-07 19:12:49 +0100

    correction controllen and view bookingd

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,29 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @buoy = Buoy.find(params[:buoy_id])
   end
 
   def create
     @buoy = Buoy.find(params[:buoy_id])
     @booking = Booking.new(bookings_params)
     @booking.buoy = @buoy
-    @booking.user = user
+    @booking.user = current_user
     if @booking.save
-      @booking.save
+      @booking.save!
       redirect_to buoy_path(@buoy)
     else
       @booking = Booking.new
       render "buoys/show"
     end
+  end
 
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
   end
+
+  private
 
   def bookings_params
     params.require(:booking).permit(:start_date, :end_date)
