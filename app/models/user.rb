00b1d1486334b778abb24807e28c79830bd64270
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-07 12:07:02 +0100

    added user validations first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   has_many :reviews, through: :bookings
   has_many :photos
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
