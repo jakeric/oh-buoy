28cc679a27c65d7454146c2739f0e7e593e8fee5
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-07 14:56:40 +0100

    added user reference to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_131850) do
+ActiveRecord::Schema.define(version: 2018_08_07_132926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,9 @@ ActiveRecord::Schema.define(version: 2018_08_07_131850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "booking_id"
+    t.bigint "user_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,4 +94,5 @@ ActiveRecord::Schema.define(version: 2018_08_07_131850) do
   add_foreign_key "photos", "buoys"
   add_foreign_key "photos", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
