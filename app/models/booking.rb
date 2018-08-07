7b1758b14402ae0b5a615da34918a3d8f5aba509
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-07 12:39:50 +0100

    added booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,20 +1,19 @@
 class Booking < ApplicationRecord
+  validate :end_after_start
   belongs_to :buoy
   belongs_to :user
   has_one :review
 
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
-  # validates :buoy_id, presence: true
-  # validates :user_id, presence: true
-  # validate :end_after_start
+  validates :start_date, presence: true
+  validates :end_date, presence: true #, date: { after_or_equal_to:  :start_date}
+
 
   private
 
   def end_after_start
     return if end_date.blank? || start_date.blank?
 
-    if end_date <= start_date
+    if end_date < start_date
       errors.add(:end_date, "must be after the start date")
     end
   end
