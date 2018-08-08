8ed0311dd106e8f7ce149cee1beebfb38753e1e9
Scarlett Wolter <scarlett.wolter@web.de>
2018-08-08 16:10:48 +0100

    view create new boy and navbar styling

--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 2018_08_08_105939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_buoys_on_user_id"
   end
 
