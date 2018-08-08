d2b66d1a65b23d9dcf4b9976b976f34e5fee2e9f
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-08 13:30:39 +0100

    my first css

--- app/models/buoy.rb
+++ app/models/buoy.rb
@@ -9,13 +9,5 @@ class Buoy < ApplicationRecord
   validates :category, presence: true, inclusion: { in: ["Food", "Animal", "Unicorn", "Cartoon", "Classic", "Weird"] }
   validates :capacity, presence: true
   validates :price_per_day, numericality: true, presence: true
-<<<<<<< HEAD
-<<<<<<< HEAD
   validates :photo, presence:true
-=======
->>>>>>> b3dd182e895f9ae6886fa1af63135052b3156cb6
-=======
-  validates :photo, presence:true
-
->>>>>>> 682efadc4cf232460d6d9ca1e7e2cba361a10c71
 end
