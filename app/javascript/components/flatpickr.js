f67671aa24e5c3930a5cc544178962cc3dd82906
Kaj Schattner <kaj.schattner@googlemail.com>
2018-08-10 15:46:24 +0100

    datepicker advanced

--- app/javascript/components/flatpickr.js
+++ app/javascript/components/flatpickr.js
@@ -1,26 +1,20 @@
 import flatpickr from 'flatpickr';
 
 const toggleDateInputs = function() {
+  const disabledDates = document.querySelector('.buoy-show-datepicker').getAttribute('disable-dates');
   const startDateinput = document.getElementById('booking_start_date');
   const endDateinput = document.getElementById('booking_end_date');
 
-  if (startDateinput && endDateinput) {
-    flatpickr(startDateinput, {
+  flatpickr(startDateinput, {
+    mode: "range",
     minDate: 'today',
-    dateFormat: 'd-m-Y',
+    dateFormat: "Y-m-d",
+    disable: JSON.parse(disabledDates),
     onChange: function(_, selectedDate) {
-      if (selectedDate === '') {
-        return endDateinput.disabled = true;
-      }
-      endDateCalendar.set('minDate', selectedDate);
-      endDateinput.disabled = false;
-    }
-  });
-    const endDateCalendar =
-      flatpickr(endDateinput, {
-        dateFormat: 'd-m-Y',
-      });
-  }
+      let splitted = selectedDate.split(" to ");
+      startDateinput.value = splitted[0];
+      endDateinput.value = splitted[1];
+    }});
 };
 
 export { toggleDateInputs }
